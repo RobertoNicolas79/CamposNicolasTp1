d65f8dbfd3291277d25fed8b78c28eb1ab082fc1
RobertoNicolas79 <agusstinn1979@gmail.com>
2021-04-29 09:37:40 -0300

    First Commit

---
+++ ArbolGeneral.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tp1
+{
+	public class ArbolGeneral<T>
+	{
+		
+		private T dato;
+		private List<ArbolGeneral<T>> hijos = new List<ArbolGeneral<T>>();
+		
+
+		public ArbolGeneral(T dato) {
+			this.dato = dato;
+		}
+	
+		public T getDatoRaiz() {
+			return this.dato;
+		}
+	
+		public List<ArbolGeneral<T>> getHijos() {
+			return hijos;
+		}
+	
+		public void agregarHijo(ArbolGeneral<T> hijo) {
+			this.getHijos().Add(hijo);
+		}
+	
+		public void eliminarHijo(ArbolGeneral<T> hijo) {
+			this.getHijos().Remove(hijo);
+		}
+	
+		public bool esHoja() {
+			return this.getHijos().Count == 0;
+		}
+		
+		public void preOrden(){
+			foreach(var hijo in this.getHijos())
+				hijo.preOrden();
+			
+			Console.Write("["+this.getDatoRaiz()+"]");
+				
+		}
+		
+		public int cantHojas(){
+			int hojas=9;
+			if(this.esHoja()==true){
+				hojas++;
+			}
+
+			foreach(var hijo in this.getHijos())
+				hijo.cantHojas();
+				
+			
+			return hojas;
+		}
+	
+		//public int altura() {
+			//return 0;
+		//}
+		
+		
+		//public int nivel(T dato) {
+			//return 0;
+		//}
+	
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tp1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			ArbolGeneral<int> arbol1 = new ArbolGeneral<int>(1);
+			
+			ArbolGeneral<int> arbol2 = new ArbolGeneral<int>(2);
+			ArbolGeneral<int> arbol3 = new ArbolGeneral<int>(3);
+			ArbolGeneral<int> arbol4 = new ArbolGeneral<int>(4);
+			
+			ArbolGeneral<int> arbol5 = new ArbolGeneral<int>(5);
+			ArbolGeneral<int> arbol6 = new ArbolGeneral<int>(6);
+			ArbolGeneral<int> arbol7 = new ArbolGeneral<int>(7);
+		
+			ArbolGeneral<int> arbol8 = new ArbolGeneral<int>(8);
+			ArbolGeneral<int> arbol9 = new ArbolGeneral<int>(9);
+			ArbolGeneral<int> arbol10 = new ArbolGeneral<int>(10);
+			
+			ArbolGeneral<int> arbol11 = new ArbolGeneral<int>(11);
+			ArbolGeneral<int> arbol12 = new ArbolGeneral<int>(12);
+			ArbolGeneral<int> arbol13 = new ArbolGeneral<int>(13);
+			
+			arbol1.agregarHijo(arbol2);
+			arbol1.agregarHijo(arbol3);
+			arbol1.agregarHijo(arbol4);
+			
+			arbol2.agregarHijo(arbol5);
+			arbol2.agregarHijo(arbol6);
+			arbol2.agregarHijo(arbol7);
+			
+			arbol3.agregarHijo(arbol8);
+			arbol3.agregarHijo(arbol9);
+			arbol3.agregarHijo(arbol10);
+			
+			arbol4.agregarHijo(arbol11);
+			arbol4.agregarHijo(arbol12);
+			arbol4.agregarHijo(arbol13);
+		
+			arbol1.preOrden();
+			Console.WriteLine("\n"+arbol1.cantHojas());
+			
+			
+			Console.Write("\nPress any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
